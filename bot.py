fc5065e7b4ab3d03d150aac6f40f7a2bf7c1361d
Andrew Dolgikh <internet-services@yandex.ru>
2020-03-21 13:52:41 +0300

    deploy_21032020_1352

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import redis
+# import redis
 import os
 import telebot
 # import some_api_lib
@@ -12,9 +12,15 @@ some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+# r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
-# bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
