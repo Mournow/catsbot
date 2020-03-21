1913f5b8942c6d7f07c81c33fa5b077bacd7a7da
Andrew Dolgikh <internet-services@yandex.ru>
2020-03-21 14:35:36 +0300

    grgvrrgr

--- bot.py
+++ bot.py
@@ -26,6 +26,6 @@ def start_message(message):
     
     img = open(os.path.join(os.path.abspath(os.curdir), 'cats/s7zN3CUtdPA.jpg'), 'rb')
 
-    bot.send_photo(message.chat.id, img, reply_to_message_id=message.chat.id)
+    bot.send_photo(message.chat.id, img, reply_to_message_id=message.message_id)
 
 bot.polling()
