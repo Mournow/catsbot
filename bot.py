7c467ab47818b5ae85e4125c27f0c4a2def914f7
Andrew Dolgikh <internet-services@yandex.ru>
2020-03-22 13:27:41 +0300

    Сменил алгоритм анализа на коэффициент Танимото

--- bot.py
+++ bot.py
@@ -70,11 +70,22 @@ def start_message(message):
         }
     }
 
+    # анализ через difflib
     def similarity(s1, s2):
         normalized1 = s1.lower()
         normalized2 = s2.lower()
         matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
         return matcher.ratio()
+
+    # коэффициент танимото
+    def tanimoto(s1, s2):
+        a, b, c = len(s1), len(s2), 0.0
+
+        for sym in s1:
+            if sym in s2:
+                c += 1
+
+        return c / (a + b - c)
 
     def send_cat(url):
         try:
@@ -97,11 +108,11 @@ def start_message(message):
         strx = strx.replace(":", "")
         strx = strx.replace("-", "")
         return strx
-        
+
 
     # скоринг
     for url in scoring_dict.keys():
-        score = similarity(clear(scoring_dict[url]['meme_tag']), clear(message.text))
+        score = tanimoto(clear(scoring_dict[url]['meme_tag']), clear(message.text))
         scoring_dict[url]['scoring'] = score
         scoring_scores.append(score)
 
